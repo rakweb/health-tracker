3e872d093b92d9e0f39a9092e18cac957c4277bd
rakweb <rakweb@users.noreply.github.com>
2026-04-28 07:52:26 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,12 @@
-const CACHE_NAME = `health-tracker-${self.registration.scope}`;
-const VERSION = self.location.search;
-
-self.addEventListener('install', () => {
+self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', (e) => {
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  // Network-first, no caching surprises
+  event.respondWith(fetch(event.request));
 });
-``
